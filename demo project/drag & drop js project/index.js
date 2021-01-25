6f252049521dd9766bc9bce75a3fc01251be4dee
vidya-sri <vidhya.srilek@gmail.com>
2021-01-25 19:25:45 +0530

    commit

--- demo project/drag & drop js project/index.js
+++ demo project/drag & drop js project/index.js
@@ -3,24 +3,28 @@ console.log('This is drag and drop utility');
 const imgBox = document.querySelector('.imgBox');
 const whiteBoxes = document.getElementsByClassName('whiteBox');
 
-// Event listeners for draggable element imgBox
+// Event listeners for draggable element imgBox and 
+//'e' is a event in which 'hold' class should be added
+// space in fornt of ' hold'should be given 
+//because when we give one element a multiple class then we need separate by giving space.
+// example for one element you needed to 3 class then class = "row hold"
 imgBox.addEventListener('dragstart', (e) => {
     console.log('DragStart has been triggered');
-    e.target.className += ' hold';
+    e.target.className += ' hold'; // we are writting because we are 'adding hold class' to the 'event'.
     setTimeout(() => {
         e.target.className = 'hide';
-    }, 0);
+    }, 0);// 0 is used because it should hide at the end of event
 
 });
 
 imgBox.addEventListener('dragend', (e) => {
     console.log('DragEnd has been triggered');
-    e.target.className = 'imgBox';
+    e.target.className = 'imgBox'; // doing red box disappear
 });
 
 for (whiteBox of whiteBoxes) {
     whiteBox.addEventListener('dragover', (e) => {
-        e.preventDefault();
+        e.preventDefault();// doing this we can drop events in boxs
         console.log('DragOver has been triggered');
     });
 
