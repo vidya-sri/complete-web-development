27ed0f146efef768b74affdaeeb2947a112d3400
vidya-sri <vidhya.srilek@gmail.com>
2021-01-25 18:25:06 +0530

    calculator project

---
+++ demo project/calculator/cal.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+//screenvalue is 'string' and screen.value is input value//
+let screenValue = '';
+for(item of buttons){ 
+   item.addEventListener('click' , (e)=>{
+        buttonText = e.target.innerText;
+        console.log('Button Text is', buttonText);
+        if(buttonText =='X'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'C'){
+          screenValue = "";
+          screen.value = screenValue;
+        }
+        else if(buttonText == '='){
+            screen.value = eval(screenValue);
+          }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+
+
+
+    })
+}
